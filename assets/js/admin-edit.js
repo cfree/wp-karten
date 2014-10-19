650381c947be411c5a61fdd192ad5a9f23c5bdd8
Craig Freeman <craig@crowdfavorite.com>
2014-10-19 15:56:05 -0600

    Updated logic in admin scripts

--- assets/js/admin-edit.js
+++ assets/js/admin-edit.js
@@ -1,7 +1,7 @@
 (function($) {
 	$(document).ready(function() {
 		// Check to make sure the input box exists
-		if ($('.datepicker').length) {
+		if ($('.datepicker').length > 0) {
 			// Initilaize datepicker
 			$('.datepicker').datepicker();
 		}
